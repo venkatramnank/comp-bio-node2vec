1174f2704f944ca025b4c7ab0e9c7b4a66ddf02d
Venkat Ramnan Kalyanakumar <venkatramnankalyanakumar@Venkats-MacBook-Air.local>
2024-03-16 21:22:57 -0700

    (doc) documented class GraphEDA

--- analysis/exploratory_data_analysis.py
+++ analysis/exploratory_data_analysis.py
@@ -11,29 +11,57 @@ from scripts import constants
 FILE_LOCATION = neuron_cc_config.FILE_LOCATION
 
 
-class graphEDA():
+class GraphEDA:
+    """Class for performing exploratory data analysis (EDA) on a graph."""
+
     def __init__(self, file_location, file_type, show_head=False):
+        """
+        Initialize the GraphEDA object.
+
+        Parameters:
+        - file_location (str): The path to the input file.
+        - file_type (str): The type of the input file (e.g., 'csv', 'tsv').
+        - show_head (bool): Whether to display the head of the DataFrame.
+
+        """
         self.file_location = file_location
         self.file_type = file_type
         self.show_head = show_head
         self.data_df = self.data_reader()
-        self.data_graph = self.graph_builer()
+        self.data_graph = self.graph_builder()
 
     def data_reader(self):
+        """
+        Read data from the input file and return it as a DataFrame.
+
+        Returns:
+        - dataframe (pd.DataFrame): The DataFrame containing the data.
+
+        """
         logger.debug('Reading File content')
-        dataframe = pd.read_csv(self.file_location, sep=constants.file_seperator[self.file_type])
+        dataframe = pd.read_csv(self.file_location, sep=constants.file_separator[self.file_type])
         if self.show_head:
             print(tabulate(dataframe.head(), tablefmt='fancy_grid'))
         return dataframe
     
-    def graph_builer(self):
+    def graph_builder(self):
+        """
+        Build a graph from the DataFrame.
+
+        Returns:
+        - g (igraph.Graph): The constructed graph.
+
+        """
         logger.debug('Building graph')
         g = ig.Graph.TupleList(self.data_df.itertuples(index=False), directed=False)
         print(g.summary())
         return g
 
-    
     def graph_stats(self):
+        """
+        Print basic statistics about the graph.
+
+        """
         # Basic graph statistics
         print("Basic Graph Statistics:")
         print("Number of nodes:", self.data_graph.vcount())
--- configs/neuron_cc_config.py
+++ configs/neuron_cc_config.py
@@ -1,1 +1,3 @@
+# Download from https://snap.stanford.edu/biodata/datasets/10023/10023-CC-Neuron.html
+
 FILE_LOCATION = "/Users/venkatramnankalyanakumar/Desktop/OSU/winter2024/NetworksCompBio/project/comp-bio-project/data/CC-Neuron_cci.tsv"
